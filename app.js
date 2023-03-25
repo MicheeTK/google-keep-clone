98fef841413d5299b2e1a89f056a1697116873ea
Michee_Tokatoka <tokatokakiyombo@gmail.com>
2023-03-25 20:36:20 +0200

    added event listeners to toggle btw active and inactive forms

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-alert("hey");
 class Note {
   constructor(id, title, text) {
     this.id = id;
@@ -10,6 +9,32 @@ class Note {
 class App {
   constructor() {
     this.notes = [];
+    this.$activeForm = document.querySelector(".active-form");
+    this.$inActiveForm = document.querySelector(".inactive-form");
+    this.$noteTitle = document.querySelector(".note-title");
+    this.$inputNote = document.querySelector(".input-note");
+
+    this.addEventListeners();
+  }
+
+  addEventListeners() {
+    document.body.addEventListener("click", (event) => {
+      this.handleFormClick(event);
+    });
+  }
+
+  handleFormClick(event) {
+    const isActiveFormClicked = this.$activeForm.contains(event.target);
+    const isinActiveFormClicked = this.$inActiveForm.contains(event.target);
+
+    if (isinActiveFormClicked) {
+      this.$inActiveForm.style.display = "none";
+      this.$activeForm.style.display = "flex";
+      this.$inputNote.focus();
+    } else if (!isinActiveFormClicked) {
+      this.$inActiveForm.style.display = "flex";
+      this.$activeForm.style.display = "none";
+    }
   }
 
   addNote(id, { title, text }) {
